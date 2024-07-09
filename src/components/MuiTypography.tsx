1a0b4bf60137ebef6f5500eec243fe43f9bb3b61
Ed <tech.tatay@gmail.com>
2024-07-09 20:34:35 +0800

    material UI Exercise1

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,38 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>H1 heading</Typography>
+      <Typography variant='h2'>H2 heading</Typography>
+      <Typography variant='h3'>H3 heading</Typography>
+      <Typography variant='h4' gutterBottom>H4 heading</Typography>
+      <Typography variant='h5'>H5 heading</Typography>
+      <Typography variant='h6'>H6 heading</Typography>
+
+      <Typography variant='subtitle1'>Subtitle 1</Typography>
+      <Typography variant='subtitle2'>Subtitle 2</Typography>
+
+      <Typography variant='body1'>Lorem ipsum dolor sit amet, consectetur 
+       elit, sed do eiusmod tempor incididunt ut 
+       labore et dolore magna aliqua. Ut enim ad minim veniam, 
+       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+       fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+       sunt in culpa qui officia deserunt mollit anim id est laborum. 
+       </Typography>
+
+       
+      <Typography variant='body2'>Lorem ipsum dolor sit amet, consectetur 
+       elit, sed do eiusmod tempor incididunt ut 
+       labore et dolore magna aliqua. Ut enim ad minim veniam, 
+       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+       fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+       sunt in culpa qui officia deserunt mollit anim id est laborum. 
+       </Typography>
+
+
+    </div>
+  )
+}
